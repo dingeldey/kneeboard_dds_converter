62552689e4f48207866cb104a64df6ddf1691316
dingeldey <94619323+dingeldey@users.noreply.github.com>
2021-12-04 16:03:13 +0100

    added logging to app

--- convert_to_kneeboard.py
+++ convert_to_kneeboard.py
@@ -6,7 +6,7 @@ import os.path
 from wand import image as WandImage
 from PIL import Image as PILImage
 import traceback
-
+from submodules.python_core_libs.logging.project_logger import Log
 
 def convert_to_kneeboard(file_name: str, kneeboard_page: int, out_dir: str, logger=None):
     """
@@ -44,14 +44,64 @@ def convert_to_kneeboard(file_name: str, kneeboard_page: int, out_dir: str, logg
         os.remove(tmp_file)
 
 
+
+
+def create_logger_ini():
+    logger_ini_content = """[formatters]
+keys=default
+
+[formatter_default]
+format=<%(levelname)-3s><%(asctime)s> %(message)s <%(filename)s:%(lineno)d>'
+class=logging.Formatter
+
+[handlers]
+keys=console, file
+
+[handler_console]
+class=logging.StreamHandler
+formatter=default
+args=tuple()
+
+[handler_file]
+class=logging.FileHandler
+level=INFO
+formatter=default
+args=("convert_to_kneeboard.log", "w")
+
+[loggers]
+keys=root
+
+[logger_root]
+level=INFO
+formatter=default
+handlers=console,file"""
+
+    ini_exists: bool = os.path.isfile("convert_to_kneeboard_logger.ini")
+    if not ini_exists:
+        with open("convert_to_kneeboard_logger.ini", 'w') as f:
+            f.write(logger_ini_content)
+
+
+def remove_logger_ini():
+    ini_exists: bool = os.path.isfile("convert_to_kneeboard_logger.ini")
+    if ini_exists:
+        os.remove("convert_to_kneeboard_logger.ini")
+
+
 if __name__ == "__main__":
+    create_logger_ini()
+    logger = Log.instance().logger
+    Log.instance().set_ini("convert_to_kneeboard_logger.ini")
+    logger.info("Start")
     import glob
     files = glob.glob('./*.png')
     files = files + glob.glob('./*.jpg')
 
     for i, file in enumerate(files):
         try:
-            convert_to_kneeboard(file, i+1, './')
+            convert_to_kneeboard(file, i+1, './', logger)
         except Exception as e:
             print(str(e))
             print("\n".join(traceback.TracebackException.from_exception(e).format()))
+
+    remove_logger_ini()
