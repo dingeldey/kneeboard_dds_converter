984cc70e2d76bbebc5f15282ee1ec9cb9ed9c866
dingeldey <94619323+dingeldey@users.noreply.github.com>
2021-12-04 16:07:30 +0100

    make a 7z and zip archieve, removed self-extracting archieve

--- buildrelease.ps1
+++ buildrelease.ps1
@@ -10,6 +10,7 @@ New-Item -Path ".\" -Name "kneeboard_dds_converter" -ItemType "directory"
 Copy-Item .\README.md -Destination ".\kneeboard_dds_converter"
 Copy-Item -Path ".\dist\convert_to_kneeboard.exe" -Destination ".\kneeboard_dds_converter" -Recurse
 
- & 'C:\Program Files\7-Zip\7z.exe' a -mx9 -sfx .\release\kneeboard_dds_converter.exe kneeboard_dds_converter
+# & 'C:\Program Files\7-Zip\7z.exe' a -mx9 -sfx .\release\kneeboard_dds_converter.exe kneeboard_dds_converter
  & 'C:\Program Files\7-Zip\7z.exe' a -t7z .\release\kneeboard_dds_converter.7z kneeboard_dds_converter
+ & 'C:\Program Files\7-Zip\7z.exe' a .\release\kneeboard_dds_converter.zip kneeboard_dds_converter
 rm -r kneeboard_dds_converter
