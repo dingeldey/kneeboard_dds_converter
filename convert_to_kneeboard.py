f745cb2d6d052b265cb3daa659cad30b1446250b
Andreas Dingeldey <none@none.de>
2022-07-31 02:18:13 +0200

    Now only modifies right side of existing kneeboard page which is intentially left empty by BMS-Devs so we can write our pages in.

--- convert_to_kneeboard.py
+++ convert_to_kneeboard.py
@@ -7,10 +7,12 @@ from wand import image as WandImage
 from PIL import Image as PILImage
 import traceback
 from submodules.python_core_libs.logging.project_logger import Log
+from wand import image
 
 
-def convert_to_kneeboard(file_name: str, kneeboard_page: int, out_dir: str, logger=None):
+def convert_to_kneeboard(dest_dir: str, file_name: str, kneeboard_page: int, out_dir: str, logger=None):
     """
+    :param dest_dir: destination directory where the dds goes in the end
     :param file_name: Kneeboard png to be converted
     :param kneeboard_page:  which kneeboard page shall it become
     :param out_dir: path to which dds is written.
@@ -26,25 +28,24 @@ def convert_to_kneeboard(file_name: str, kneeboard_page: int, out_dir: str, logg
     # HACK: I do not exactly know how many pages should be replaced, but before writing over needed data, I chose 17 for now.
     # If you run into this restriction, we will have to figure it out :)
     assert 0 < kneeboard_page < 17
-    tmp_file = os.path.join(out_dir, "tmp.png").replace("\\", "/")
     with PILImage.open(file_name) as img:
-        resized_image = img.resize((2048, 2048))
+        resized_image = img.resize((1024, 2048))
         # Save the cropped image
-        resized_image.save(tmp_file)
+        resized_image.save(file_name)
 
-    with WandImage.Image(filename=tmp_file) as img:
-        page1 = 7982
-        page = page1 - 1 + kneeboard_page
-        img.compression = 'dxt5'
-        dds_file = os.path.join(out_dir, str(page) + '.dds')
-        if logger is not None:
-            logger.info(f"create kneeboard {dds_file}")
-        img.save(filename=dds_file)
-
-    if os.path.exists(tmp_file):
-        if logger is not None:
-            logger.info("Removing tmp.png")
-        os.remove(tmp_file)
+    page1 = 7982
+    page = page1 - 1 + kneeboard_page
+    bkp_dds = os.path.join(dest_dir, str(page) + '.dds_bkp')
+    logger.info(f"Trying to open orirignal dds {bkp_dds}")
+    with image.Image(filename=bkp_dds) as left:
+        img.compression = "no"
+        with image.Image(filename=os.path.join(out_dir, str(kneeboard_page) + '.png')) as right:
+            right.resize(1024, 2048)
+            with image.Image(width=2048, height=2048) as output:  # make a 2048
+                output.composite(image=left, left=0, top=0)
+                output.composite(image=right, left=1024, top=0)
+                output.compression = 'dxt5'
+                output.save(filename=os.path.join(dest_dir, str(page) + '.dds'))
 
 
 if __name__ == "__main__":
